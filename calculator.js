a8b83a60b6d880f9b9831ebe547d43cb56621a44
Rudra Arindom Niloy <159453082+nil3939@users.noreply.github.com>
2025-02-18 21:30:23 +0600

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// Simple calculator 
+let num1 = 8;
+let num2 = 4;
+
+function calculator(a, b) {
+  console.log("Add: " + (a + b));
+  console.log("Subtract: " + (a - b));
+  console.log("Multiply: " + (a * b));
+  console.log("Divide: " + (a / b));
+}
+
+calculator(num1, num2);
